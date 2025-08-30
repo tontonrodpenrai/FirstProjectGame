b0f9da93a10f4de18fafc279fdfd7665d0303ce0
tontonrodpenrai <panitpicha.rang@mail.kmutt.ac.th>
2025-08-30 16:00:15 +0700

    add

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -16,25 +16,22 @@ var definePlugin = new webpack.DefinePlugin({
 
 module.exports = {
   entry: {
-    app: [
-      path.resolve(__dirname, 'src/main.js')
-    ],
-
+    app: [path.resolve(__dirname, "src/main.js")],
   },
   output: {
-    path: path.resolve(__dirname, 'build'),
-    publicPath: './',
-    filename: 'js/bundle.js'
+    path: path.resolve(__dirname, "build"),
+    publicPath: "./",
+    filename: "js/bundle.js",
   },
   plugins: [
     definePlugin,
-    new CleanWebpackPlugin(['build']),
+    new CleanWebpackPlugin(["build"]),
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
     new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: './src/index.html',
-      chunks: ['vendor', 'app'],
-      chunksSortMode: 'manual',
+      filename: "index.html",
+      template: "./src/index.html",
+      chunks: ["vendor", "app"],
+      chunksSortMode: "manual",
       minify: {
         removeAttributeQuotes: true,
         collapseWhitespace: true,
@@ -43,22 +40,26 @@ module.exports = {
         minifyJS: true,
         minifyURLs: true,
         removeComments: true,
-        removeEmptyAttributes: true
+        removeEmptyAttributes: true,
       },
-      hash: true
+      hash: true,
     }),
-    new CopyWebpackPlugin([
-      { from: './assets', to: './assets'}
-    ])
+    new CopyWebpackPlugin({
+      patterns: [{ from: "./assets", to: "./assets" }],
+    }),
   ],
   module: {
     rules: [
-      { test: /\.js$/, use: ['babel-loader'], include: path.join(__dirname, 'src') },
-      { test: /phaser-split\.js$/, use: 'raw-loader' },
-      { test: [/\.vert$/, /\.frag$/], use: 'raw-loader' }
-    ]
+      {
+        test: /\.js$/,
+        use: ["babel-loader"],
+        include: path.join(__dirname, "src"),
+      },
+      { test: /phaser-split\.js$/, use: "raw-loader" },
+      { test: [/\.vert$/, /\.frag$/], use: "raw-loader" },
+    ],
   },
   optimization: {
-    minimize: true
+    minimize: true,
   },
-}
+};
